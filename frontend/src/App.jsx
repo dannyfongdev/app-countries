904317a4329a4915a6eb79ef7cfbf793561d5fef
Danny Fong <danny.fong.dev@outlook.com>
2023-03-08 14:31:35 -0800

    Region filter functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,9 +8,10 @@ import SearchBar from "./components/SearchBar";
 
 function App() {
   const [searchTerm, setSearchTerm] = useState();
+  const [regionTerm, setRegionTerm] = useState();
   const [showGrid, setShowGrid] = useState(true);
-  const [countries, setCountries] = useState();
-  const [detailCountry, setDetailCountry] = useState();
+  const [countries, setCountries] = useState(); // list of country objects passed to CountryGrid component
+  const [detailCountry, setDetailCountry] = useState(); // country code passed to CountryDetail component
 
   // on load, default to region = 'Americas'
   useEffect(() => {
@@ -30,7 +31,20 @@ function App() {
 
   function handleSearch(s) {
     setSearchTerm(s);
+    setRegionTerm('');
     getCountry(s)
+      .then (data => {
+        setCountries(data);
+      })
+      .catch (error => {
+        console.log(error);
+      });
+  }
+
+  function handleRegion(r) {
+    setSearchTerm('');
+    setRegionTerm(r);
+    getRegion(r)
       .then (data => {
         setCountries(data);
       })
@@ -41,10 +55,11 @@ function App() {
 
   return (
     <div className="p-6 bg-slate-200">
-      {showGrid && <SearchBar onSearch={s => handleSearch(s)} onSelectRegion={r => console.log('App', r)} />}
-      {showGrid && searchTerm && <div className="mb-4 font-bold">Searching for "{searchTerm}"</div>}
+      {showGrid && <SearchBar onSearch={handleSearch} onSelectRegion={handleRegion} />}
+      {showGrid && searchTerm && <div className="mb-4 font-bold">Search: "{searchTerm}"</div>}
+      {showGrid && regionTerm && <div className="mb-4 font-bold">Region: "{regionTerm}"</div>}
       {!showGrid && <CountryDetail code={detailCountry} onBack={() => setShowGrid(true)} />}
-      {showGrid && <CountryGrid onShow={cca3 => showCountryDetail(cca3)} countries={countries}/>}
+      {showGrid && <CountryGrid onShow={showCountryDetail} countries={countries}/>}
     </div>
   );
 }
--- frontend/src/components/DropdownMenu.jsx
+++ frontend/src/components/DropdownMenu.jsx
@@ -1,21 +1,28 @@
-// copied from flowbite, need to make functional
+import { useState } from 'react';
 
 function DropdownMenu(props) {
   const { onSelectRegion } = props;
+  const [showMenu, setShowMenu] = useState(false);
 
-  const handleSelect = () => {
-    onSelectRegion('Asia');
+  const handleSelect = (r) => {
+    onSelectRegion(r);
+    handleToggle();
   }
+
+  const handleToggle = () => {
+    setShowMenu(!showMenu);
+  }
+
   return (
     <div>
       <button
         id="dropdownDefaultButton"
         data-dropdown-toggle="dropdown"
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        className="relative text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         type="button"
-        onClick={handleSelect}
+        onClick={handleToggle}
       >
-        Dropdown button{" "}
+        Filter by Region{" "}
         <svg
           className="w-4 h-4 ml-2"
           aria-hidden="true"
@@ -35,44 +42,28 @@ function DropdownMenu(props) {
 
       <div
         id="dropdown"
-        className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
+        className={(showMenu ? 'block' : 'hidden') + " absolute z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 cursor-pointer"}
       >
         <ul
           className="py-2 text-sm text-gray-700 dark:text-gray-200"
           aria-labelledby="dropdownDefaultButton"
         >
-          <li>
-            <a
-              href="#"
-              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              Dashboard
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              Settings
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              Earnings
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              Sign out
-            </a>
-          </li>
+          <li><a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" onClick={()=>handleSelect('Africa')}>
+              Africa
+          </a></li>
+          <li><a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" onClick={()=>handleSelect('Americas')}>
+              Americas
+          </a></li>
+          <li><a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" onClick={()=>handleSelect('Asia')}>
+              Asia
+          </a></li>
+          <li><a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" onClick={()=>handleSelect('Europe')}>
+              Europe
+          </a></li>
+          <li><a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" onClick={()=>handleSelect('Oceania')}>
+              Oceania
+          </a></li>
+
         </ul>
       </div>
     </div>
